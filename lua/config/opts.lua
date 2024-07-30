b98cc34c45e7a55027b5a5be37f37de7bf77871d
Samyabrata Maji <samyabratamaji334@gmail.com>
2024-07-30 14:00:20 +0530

    toggleterm
    Signed-off-by: Samyabrata Maji <samyabratamaji334@gmail.com>

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -24,6 +24,9 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
